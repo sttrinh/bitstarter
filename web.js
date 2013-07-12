0226c39895544512e887728e8a9e5ce7800ae966
Stephen Trinh <stephentrinh88@gmail.com>
2013-07-12 07:56:52 +0000

    Changed web.js to use index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fileBuffer = fs.readFileSync('index.html');
+  response.send(fileBuffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
